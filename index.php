0eb7aedcc77ac7a43c1719b69c3b0e29c98044d2
profAlbertoAyres <alberto.benicio@ifro.edu.br>
2025-05-20 23:35:42 -0400

    Correçoes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php $arquivo = basename(__FILE__);?>
+<?php $arquivo = basename(__FILE__); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,31 +13,42 @@
 </head>
 
 <body>
+
     <header>
         <?php include '_parts/_cabecalho_site.php' ?>
     </header>
-
+    <?php
+    spl_autoload_register(function ($class) {
+        require_once "classes/{$class}.class.php";
+    });
+    $inicial = new Inicial;
+    ?>
     <main>
-
+        <?php
+        $conteudos = $inicial->all();
+        foreach ($conteudos as $cont):
+            $campo = 'categoriaproduto';
+            ?>
             <section class="sec-branco">
 
-               
+                <h2><?php echo $cont->tituloinicial ?></h2>
                 <hr class="border border-success border-1 opacity-75">
                 <div class="linha">
                     <div class="item">
-                        
+                        <img src="images/inicial/<?php echo $cont->imageminicial ?>" alt="">
                     </div>
 
                     <div class="item">
                         <p>
-                            
+                            <?php echo $cont->textoinicial ?>
                         </p>
                     </div>
                 </div>
 
             </section>
-    
+        <?php endforeach; ?>
     </main>
+
     <footer>
         <?php include "_parts/_footer.php" ?>
     </footer>
